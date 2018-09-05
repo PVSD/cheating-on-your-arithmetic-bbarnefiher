1c645cea6a50eb4bf5ebebf446ef6277c487cf0e
BuildTools <unconfigured@null.spigotmc.org>
2018-09-05 16:07:16 -0400

    done at not 1 am

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String equationOne = ("79 + 3 * (4 + 82 - 68) - 7 +19");
+        int answerOne = 79 + 3 * (4 + 82 - 68) - 7 +19;
+        String equationTwo = ("(179 +21 +10)/ 7 + 181");
+        int answerTwo = (179 +21 +10)/ 7 + 181;
+        String equationThree = ("10389 * 56 * 11 + 2246");
+        int answerThree = 10389 * 56 * 11 + 2246;
+
+        System.out.println(equationOne + " = " + answerOne);
+        System.out.println(equationTwo + " = " + answerTwo);
+        System.out.println(equationThree + " = " + answerThree);
+
     }
 }
